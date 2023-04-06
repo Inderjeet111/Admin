84bada0fd0928f0e1eb3bccdf5d44a288aa5b8cf
Inderjeet Singh <inderjeet@signitysolutions.in>
2023-04-06 18:54:34 +0530

    charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+ 
+  public pieChartLabels = [ 'Active Users', 'Inactive Users','Rejected Users'];
+  public pieChartData = [5, 3,2];
+  public pieChartColors = [
+    {
+        backgroundColor: [
+            '#645CBB',
+            '#A084DC',
+            '#BFACE2'
+        ],
+        hoverBackgroundColor:[
+            '#645CBB',
+            '#A084DC',
+            '#BFACE2'
+        ]
+    }
+];
 }
