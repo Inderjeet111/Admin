b04a642a437e04be6d48574a6c5e74c079a8f1fe
ayush4444 <ayush@signitysolutions.in>
2023-04-10 12:44:13 +0530

    home screen and user-list module created

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -16,23 +16,7 @@ export class HeaderComponent {
 
   isGerman: boolean=false;
  
-  public pieChartLabels = [ 'Active Users', 'Inactive Users','Rejected Users'];
-  public pieChartData = [5, 3,2];
-  public pieChartColors = [
-    {
-        backgroundColor: [
-            '#645CBB',
-            '#A084DC',
-            '#BFACE2'
-        ],
-        hoverBackgroundColor:[
-            '#645CBB',
-            '#A084DC',
-            '#BFACE2'
-        ]
-    }
-];
-
+ 
 ngOnInit() {
   if(localStorage.getItem('selectedLang')==='de'){
     this.isGerman=false;
