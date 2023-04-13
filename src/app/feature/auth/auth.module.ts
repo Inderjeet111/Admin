868f6cfaf1efff7cecaad54279179ad8e62aefae
atul <atul@signitysolutions.in>
2023-04-13 15:48:47 +0530

    add signup and reset password form

--- src/app/feature/auth/auth.module.ts
+++ src/app/feature/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { SignupComponent } from './components/signup/signup.component';
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignupComponent,
+    ResetpasswordComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
