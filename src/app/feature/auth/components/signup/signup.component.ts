656c9fa8018806de3d05bb46146c744848e379cf
atul <atul@signitysolutions.in>
2023-04-13 16:02:06 +0530

    form

--- src/app/feature/auth/components/signup/signup.component.ts
+++ src/app/feature/auth/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,11 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
-
+signupForm:FormGroup;
   constructor(){
-
+    this.signupForm=new FormGroup({
+name:new FormControl(''),
+email:new FormControl(''),
+password:new FormControl(''),
+confirmPassword:new FormControl('')
+    })
+  
   }
-
+  
   ngOnInIt(){
 
   }
